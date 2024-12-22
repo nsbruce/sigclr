5d5d4dafa004c8bc33ed43965864b6010bd72cbf
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2024-12-22 23:42:51 +0000

    more options on generator

--- sigclr/generate_dataset.py
+++ sigclr/generate_dataset.py
@@ -8,6 +8,8 @@ import tqdm
 import numpy as np
 import click
 from sigclr.modulation_classes import SIGCLR_CLASSES
+from time import time
+
 
 @dataclass
 class SigCLRNarrowbandCleanTrainConfig(conf.NarrowbandCleanTrainConfig):
@@ -22,7 +24,7 @@ class SigCLRNarrowbandCleanValConfig(conf.NarrowbandCleanValConfig):
 @dataclass
 class SigCLRNarrowbandCleanQAConfig(conf.NarrowbandCleanTrainQAConfig):
     seed: int = 1234567893
-    num_samples: int = len(SIGCLR_CLASSES)*2
+    num_samples: int = len(SIGCLR_CLASSES) * 2
     num_iq_samples = 256
 
 num_workers = os.cpu_count() - 1
@@ -45,11 +47,29 @@ class SigCLRDatasetCreator(DatasetCreator):
 @click.option("--path", default="narrowband", help="Path to generate narrowband datasets")
 @click.option("--batch-size", type=int, default=4096, help="Batch size for dataset")
 @click.option("--num-iq-samples", type=int, help="Number of IQ samples per signal")
-def generate(path: str, batch_size: int, num_iq_samples: int) -> None:
-    print('Num IQ samples: ', num_iq_samples, '(if None means using config default)')
+@click.option("--train", is_flag=True, default=False, help="Whether to generate the training set")
+@click.option("--val", is_flag=True, default=False, help="Whether to generate the val set")
+@click.option("--qa", is_flag=True, default=False, help="Whether to generate the QA set")
+def generate(path: str, batch_size: int, num_iq_samples: int, train: bool, val: bool, qa: bool) -> None:
+
+    configs = []
+    if train:
+        configs.append(SigCLRNarrowbandCleanTrainConfig)
+    if val:
+        configs.append(SigCLRNarrowbandCleanValConfig)
+    if qa:
+        configs.append(SigCLRNarrowbandCleanQAConfig)
+
+    print('Num IQ samples:', num_iq_samples, '(if None means using config default)')
+    print('Batch size:', batch_size)    
+    print("Datasets to generate:", [c.name for c in configs])
+
+    startt = time()
     for config in [SigCLRNarrowbandCleanValConfig, SigCLRNarrowbandCleanTrainConfig]:  #,SigCLRNarrowbandCleanQAConfig]:#, 
         output_path = "{}".format(os.path.join(path, config.name))
+
         print("Building", output_path)
+
         ds = ModulationsDataset(
             classes=SIGCLR_CLASSES,
             level=config.level,
@@ -59,10 +79,13 @@ def generate(path: str, batch_size: int, num_iq_samples: int) -> None:
             include_snr=config.include_snr,
             eb_no=config.eb_no,
         )
+
         dataset_loader = DatasetLoader(ds, seed=1234567893, collate_fn=collate_fn, num_workers=num_workers, batch_size=batch_size)
         creator = SigCLRDatasetCreator(ds, seed=1234567893, path=output_path, loader=dataset_loader, num_workers=num_workers)
         creator.create()
-    
+
+    print("Done in", time()-startt, 'seconds')
+
 
 if __name__ == '__main__':
     generate()
